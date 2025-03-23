39344cd4a16ababd3e89fa706ee6d94ab9e61932
kohyeonwoo <49010769+kohyeonwoo@users.noreply.github.com>
2025-03-23 23:21:41 +0900

    250323 - 2
    플레이어 이동 수정 및 적 유형 및 관련 공격 유형 내용 추가

--- Lumberjack/Assets/Script/Enemy/Enemy.cs
+++ Lumberjack/Assets/Script/Enemy/Enemy.cs
@@ -3,37 +3,133 @@ using System.Collections.Generic;
 using UnityEngine.AI;
 using UnityEngine;
 
+public enum EnemyType { Basic, Dash, LongDistance}
+
 public class Enemy : MonoBehaviour, IDamageable
 {
 
-    protected Rigidbody rigid;
-    protected Animator anim;
-    protected Material mat;
-    protected NavMeshAgent nav;
+    public EnemyType enemyType;
 
-    protected int maxHealth;
-    protected int currentHealth;
-    protected int attackPoint;
+    private Rigidbody rigid;
+    private Animator anim;
+    private Material mat;
+    private NavMeshAgent nav;
+   
+    public int maxHealth;
+    public int currentHealth;
+    public int attackPoint;
+
+    public bool bChase;
+    public bool bAttack;
 
     public Transform target;
+    public BoxCollider enemyAttackCollision;
 
-    public void Init()
+    private void Awake()
     {
         rigid = GetComponent<Rigidbody>();
         anim = GetComponent<Animator>();
         nav = GetComponent<NavMeshAgent>();
-        mat = GetComponent<MeshRenderer>().material;    
+        enemyAttackCollision = GetComponent<BoxCollider>();
+        // mat = GetComponentInChildren<MeshRenderer>().material;    
+
+        Invoke("ChaseStart", 2.0f);
+    }
+
+    private void Update()
+    {
+        ChaseTarget();
+    }
+
+    private void FixedUpdate()
+    {
+        Targeting();
+        FreezeVelocity();
+    }
+
+    public void ChaseStart()
+    {
+        bChase = true;
+        anim.SetBool("bMove", true);
     }
 
     public void ChaseTarget()
     {
-        nav.SetDestination(target.position);
+        if(nav.enabled)
+        {
+            nav.SetDestination(target.position);
+            nav.isStopped = !bChase;
+        }  
     }
 
     protected void FreezeVelocity()
     {
-        rigid.velocity = Vector3.zero;
-        rigid.angularVelocity = Vector3.zero;
+        if(bChase)
+        {
+            rigid.velocity = Vector3.zero;
+            rigid.angularVelocity = Vector3.zero;
+        }   
+    }
+
+    protected void Targeting()
+    {
+        float targetRadius = 0.0f;
+        float targetRange = 0.0f;
+
+        switch(enemyType)
+        {
+            case EnemyType.Basic:
+                targetRadius = 1.5f;
+                targetRange = 3.0f;
+                break;
+            case EnemyType.Dash:
+                targetRadius = 1.0f;
+                targetRange = 6.0f;
+                break;
+            case EnemyType.LongDistance:
+                break;
+        }
+
+        RaycastHit[] rayHits = Physics.SphereCastAll(transform.position,
+            targetRadius,
+            transform.forward,
+            targetRange,
+            LayerMask.GetMask("Player"));
+
+        if(rayHits.Length > 0 && !bAttack)
+        {
+            StartCoroutine(Attack());
+        }
+    }
+
+    private IEnumerator Attack()
+    {
+        bChase = false;
+        bAttack = true;
+        anim.SetBool("bAttack", true);
+
+        switch(enemyType)
+        {
+            case EnemyType.Basic:
+                yield return new WaitForSeconds(0.2f);
+                enemyAttackCollision.enabled = true;
+
+                yield return new WaitForSeconds(1.0f);
+                enemyAttackCollision.enabled = false;
+
+                yield return new WaitForSeconds(1.0f);
+                break;
+            case EnemyType.Dash:
+                break;
+            case EnemyType.LongDistance:
+                break;
+        }
+
+      
+
+        bChase = true;
+        bAttack = false;
+        anim.SetBool("bAttack", false);
     }
 
     public void Damage(int Damage)
@@ -47,6 +143,15 @@ public class Enemy : MonoBehaviour, IDamageable
     }
 
     private void Dead()
+    {
+        bChase = false;
+        nav.enabled = false;
+        anim.SetTrigger("Die");
+        Invoke("EraseBody", 2.0f);
+        //this.gameObject.SetActive(false);
+    }
+
+    private void EraseBody()
     {
         this.gameObject.SetActive(false);
     }
--- Lumberjack/Assets/Script/Player/Player.cs
+++ Lumberjack/Assets/Script/Player/Player.cs
@@ -15,6 +15,8 @@ public class Player : MonoBehaviour, IDamageable
     private float hAxis;
     private float vAxis;
 
+    private bool walkDown;
+
     private Vector3 moveVector;
 
     private Animator anim;
@@ -33,18 +35,47 @@ public class Player : MonoBehaviour, IDamageable
     private void Update()
     {
 
-        hAxis = Input.GetAxisRaw("Horizontal");
-        vAxis = Input.GetAxisRaw("Vertical");
-
-        moveVector = new Vector3(hAxis, 0, vAxis).normalized;
-
-        transform.position += moveVector * speed * Time.deltaTime;
-
+        InputKey();
+        Move();
+        Turn();
+      
         if (Input.GetMouseButtonDown(0))
         {
             Attack1();
         }
     }
+
+    private void InputKey()
+    {
+        hAxis = Input.GetAxisRaw("Horizontal");
+        vAxis = Input.GetAxisRaw("Vertical");
+        walkDown = Input.GetButton("Walk");
+    }
+
+    private void Move()
+    {
+        moveVector = new Vector3(hAxis, 0, vAxis).normalized;
+
+        transform.position += moveVector * speed * (walkDown ? 0.3f : 1.0f) * Time.deltaTime;
+
+        //if(walkDown)
+        //{
+        //    this.transform.position += moveVector * speed * 0.3f * Time.deltaTime;
+        //}
+        //else
+        //{
+        //    transform.position += moveVector * speed * Time.deltaTime;
+        //} //--> if 구문 사용 방법
+
+        anim.SetBool("bRun", moveVector != Vector3.zero);
+        anim.SetBool("bWalk", walkDown);
+
+    }
+    private void Turn()
+    {
+        transform.LookAt(this.transform.position + moveVector);
+    }
+   
 
     private void Attack1()
     {
